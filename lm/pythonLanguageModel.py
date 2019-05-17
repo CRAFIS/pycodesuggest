af7776449d45f36a2f3110658bed9b10209e468f
CRAFIS <crafisbandicoot@gmail.com>
2019-05-17 19:40:33 +0900

    Update

--- lm/pythonLanguageModel.py
+++ lm/pythonLanguageModel.py
@@ -15,7 +15,6 @@ import os
 import shutil
 import tempfile
 import datetime
-import argparse
 
 from Trainer import Trainer
 from batcher import QueuedSequenceBatcher, PreBatched
@@ -248,14 +247,9 @@ if __name__ == "__main__":
     adjust_flags()
     validate_flags()
     print_flags(FLAGS)
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--mode')
-    parser.add_argument('--path')
-    args = parser.parse_args()
-    FLAGS.data_path = args.path if args.path else FLAGS.data_path
 
     for flag in mode_flags:
-        if mode_flags[flag][0] or args.mode == flag:
+        if mode_flags[flag][0]:
             print("Tensorflow version: %s" % tf.__version__)
             print("Running %s\n-------------\n" % flag)
             t0 = datetime.datetime.now()
